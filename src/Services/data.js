8a706a28cdb0bd853e19597bfdc85bca61ef0bdb
Ian <ianmchristian@gmail.com>
2022-03-20 15:05:05 -0400

    wired up spotify buttons

--- src/Services/data.js
+++ src/Services/data.js
@@ -4,7 +4,7 @@ class DataService {
     // Login to Spotify
     spotifyLogin = async() => {
         const getLogin = await http.get('/login');
-        if(!getLogin) return console.log("Error getting spotify login at DataService");
+        if(!getLogin) return console.error("Error getting spotify login at DataService");
     }
 
     // Check login
@@ -16,16 +16,48 @@ class DataService {
 
     // Currently playing + data and color scheme
     getCurrentlyPlaying = async() => {
-        const currentlyPlaying = await http.get('/currentlyplaying')
-        if(!currentlyPlaying) return console.log("Error getting currently playing at DataService");
+        const currentlyPlaying = await http.get('/currentlyplaying');
+        if(!currentlyPlaying) return console.error("Error getting currently playing at DataService");
         return currentlyPlaying;
     }
 
     // Check if there is a song playing
     checkIfSongPlaying = async() => {
-        const isPlaying = await http.get('/checkifsongisplaying')
-        if(!isPlaying) return console.log("Error checking if song playing at DataService");
+        const isPlaying = await http.get('/checkifsongisplaying');
+        if(!isPlaying) return console.error("Error checking if song playing at DataService");
         return isPlaying;
+    }
+
+    // Skip to next song
+    skipToNextTrack = async() => {
+        const nextTrackReq = await http.get('/skiptonext');
+        if(!nextTrackReq) return console.error("Error skipping to next at DataService");
+        if(nextTrackReq.status === 403) return 'Unauthorized';
+        return nextTrackReq;
+    }
+
+    // Skip to previous song
+    skipToPreviousTrack = async() => {
+        const prevTrackReq = await http.get('/skiptoprevious');
+        if(!prevTrackReq) return console.error("Error skipping to previous at DataService");
+        if(prevTrackReq.status === 403) return 'Unauthorized';
+        return prevTrackReq;
+    }
+
+    // Play/resume song
+    resumeTrack = async() => {
+        const trackReq = await http.get('/resumeplayback');
+        if(!trackReq) return console.error("Error pausing/resuming at DataService");
+        if(trackReq === 403) return 'Unauthorized';
+        return trackReq;
+    }
+
+    // Pause song
+    pauseTrack = async() => {
+        const trackReq = await http.get('/pauseplayback');
+        if(!trackReq) return console.error("Error pausing at DataService");
+        if(trackReq === 403) return 'Unauthorized';
+        return trackReq;
     }
 }
 
