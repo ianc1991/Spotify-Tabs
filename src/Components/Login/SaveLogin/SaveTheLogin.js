4b87dc9ad73439f39653c3c20cdf7f250d7a3c13
Ian <ianmchristian@gmail.com>
2022-04-07 19:17:43 -0400

    revamped how logins are handeled

---
+++ src/Components/Login/SaveLogin/SaveTheLogin.js
@@ -0,0 +1,34 @@
+import { useEffect } from 'react'
+import { useNavigate, useLocation, useSearchParams } from 'react-router-dom';
+import { Loading } from '../../Loading/Loading'
+// Service
+import data from '../../../Services/data';
+
+const SaveTheLogin = () => {
+    const navigate = useNavigate();
+    const search = useLocation().search;
+    const accessToken = new URLSearchParams(search).get('access_token');
+    const refreshToken = new URLSearchParams(search).get('refresh_token');
+
+
+
+    useEffect(() => {
+        saveLoginToLocalStorage();
+    });
+
+    const saveLoginToLocalStorage = async() => {
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        navigate('/');
+    }
+
+
+
+  return (
+    <>
+        <Loading />
+    </>
+  )
+}
+
+export default SaveTheLogin
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 // Components
 import MediaControlCard from '../Components/MediaPlayerCard/MediaPlayerCard';
+import SaveTheLogin from '../Components/Login/SaveLogin/SaveTheLogin';
 
 const Router = () => {
   
     return (
         <div>
           <Routes>
-              <Route path="" element={<MediaControlCard />} />
+            <Route path="/savelogin/" element={<SaveTheLogin />} />
+            <Route path="" element={<MediaControlCard />} />
           </Routes>
         </div>
     )
