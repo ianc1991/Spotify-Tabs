5836b82cd8bb1fb762f3746087ebb882668c0b08
Ian Christian <ianmchristian@gmail.com>
2022-08-10 17:06:16 -0400

    Fixed tab page loading text

--- src/Services/httpSpotify.js
+++ src/Services/httpSpotify.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // REPLACE ME BEFORE DEPLOY
 export default axios.create({
     baseURL: //"http://localhost:4000/",
-             "https://spotify-tabs.herokuapp.com/",
+            "https://spotify-tabs.herokuapp.com/",
     headers: {
         "Content-type": "application/x-www-form-urlencoded",
     }
